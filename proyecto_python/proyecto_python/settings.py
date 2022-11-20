c7b5f80aef261cb618b4b27f7bcff061678bea4a
Franco Cristofaro <fcristofaro@frba.utn.edu.ar>
2022-11-20 02:15:33 -0300

    agrego linea necesaria a settings para el uso de statics/

--- proyecto_python/proyecto_python/settings.py
+++ proyecto_python/proyecto_python/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for proyecto_python project.
 
@@ -116,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'prueba2_django/static/')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
