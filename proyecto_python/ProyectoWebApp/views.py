8f4b3ed6b2f274592e08bf2001abf537bfa8bf2d
Franco Cristofaro <fcristofaro@frba.utn.edu.ar>
2022-11-23 16:19:38 -0300

    cambios realizados para trabajar con tareas

--- proyecto_python/ProyectoWebApp/views.py
+++ proyecto_python/ProyectoWebApp/views.py
@@ -8,26 +8,26 @@ def home(request):
 
 def tareas(request):
     db=Database()
-    info=db.all_task()
+    info=db.all_users()
     for user in info:
         print(user[1])
     return render(request,'tareas.html',{'tareas': info})
 
 def modificar_tarea(request,id):
     db=Database()
-    task = db.get_tarea(id)
+    tarea = db.get_tarea(id)
     # prod = Item.objects.get(idproducto = id)
 
     if request.method == "POST":
         nombre_tarea_m = request.POST.get('nombre')
-        descripcion_m=request.POST.get('descripcion')
-        fecha_inicio_m = request.POST.get('fecha_inicio')
-        fecha_finalizacion_m = request.POST.get('fecha_finalizacion')
         prioridad_m = request.POST.get('prioridad')
-        db.update_tarea(id,nombre_tarea_m,descripcion_m,fecha_inicio_m,fecha_finalizacion_m,prioridad_m)
+        descripcion_m = request.POST.get('descripcion')
+        fecha_inicio_m = request.POST.get('fecha_inicio')
+        fecha_fin_m = request.POST.get('fecha_fin')
+        db.update_producto(id,nombre_tarea_m,prioridad_m,descripcion_m,fecha_inicio_m,fecha_fin_m)
         return redirect('/')
 
-    return render(request,'modificartarea.html',{'tarea': task})
+    return render(request,'modificarproducto.html',{'tarea': tarea})
 
 def crear_tarea(request):
     return render(request,'creartarea.html')
