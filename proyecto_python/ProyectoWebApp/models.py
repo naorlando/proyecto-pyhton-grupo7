98bf98932edb164e94f6baed8ac060102a2c182c
fabhri01 <rhermosaramirez@frba.utn.edu.ar>
2022-12-02 19:17:54 -0300

    querys: all task ordenada por prioridad y fecha de fin, archivar y mostrar archivadas

--- proyecto_python/ProyectoWebApp/models.py
+++ proyecto_python/ProyectoWebApp/models.py
@@ -18,7 +18,7 @@ class Database():
         self.connection = pymysql.connect(
         host='localhost',
         user='root',
-        password='1234',
+        password='',
         db='usuariospython'
     ) 
     #chequeo que la bbdd este en funcionamiento, sino no se conecta
@@ -28,13 +28,13 @@ class Database():
     
     #METODOS
     def all_task (self):
-        query ='SELECT * FROM tareas'
+        query ='SELECT * FROM tareas WHERE archivado = 0 ORDER BY prioridad_idprioridad, fecha_fin;'
 
         self.cursor.execute(query)
         tareas=self.cursor.fetchall()
         tareas=list(tareas)
         return tareas
-
+    
     def get_tarea (self, ide):
         query = "SELECT * FROM tareas WHERE idtarea = '{}'".format(ide)
 
@@ -60,8 +60,8 @@ class Database():
             raise
 
     def update_tarea(self, ide, nombre_tarea_m, prioridad_m, descripcion_m, fecha_inicio_m, fecha_fin_m):
-        query = "UPDATE tareas SET nombre_tarea = '{}',prioridad_idprioridad = (SELECT idprioridad FROM prioridad WHERE nombre_prioridad='{}'), descripcion = '{}',\
-        fecha_fin = '{}' WHERE idtarea = '{}'".format(nombre_tarea_m, prioridad_m, descripcion_m, fecha_fin_m, ide)
+        query = "UPDATE tareas SET nombre_tarea='{}', prioridad_idprioridad=(SELECT idprioridad FROM prioridad WHERE nombre_prioridad='{}'),\
+            descripcion='{}', fecha_fin='{}' WHERE idtarea = '{}'".format(nombre_tarea_m, prioridad_m, descripcion_m, fecha_fin_m, ide)
 
         try:
             self.cursor.execute(query)
@@ -72,9 +72,10 @@ class Database():
             raise
     
 
-    def create_tarea(self, nombre_tarea_m, prioridad_m, descripcion_m, fecha_inicio_m, fecha_fin_m,username_m):
-        query="INSERT INTO tareas(nombre_tarea, prioridad_idprioridad, descripcion, fecha_fin,auth_user_id)\
-        VALUES ('{}',(SELECT idprioridad FROM prioridad WHERE nombre_prioridad='{}'),'{}','{}',(SELECT id FROM auth_user WHERE username='{}'))".format(nombre_tarea_m,prioridad_m , descripcion_m, fecha_fin_m,username_m)
+    def create_tarea(self, nombre_tarea_m, prioridad_m, descripcion_m, fecha_inicio_m, fecha_fin_m, username_m):
+        query = "INSERT INTO tareas(nombre_tarea, prioridad_idprioridad, descripcion, fecha_fin,auth_user_id)\
+        VALUES ('{}',(SELECT idprioridad FROM prioridad WHERE nombre_prioridad='{}'),'{}','{}',(SELECT id FROM auth_user WHERE username='{}'))"\
+        .format(nombre_tarea_m, prioridad_m , descripcion_m, fecha_fin_m, username_m)
         
         try:
             self.cursor.execute(query)
@@ -82,7 +83,24 @@ class Database():
         except Exception:
             print("No se pudo agregar la tarea")
             raise
-        
+
+    def archivo_tarea(self, ide, valor): #Si valor = 1 se archiva, si valor = 0 se desarchiva
+        query = "UPDATE tareas SET archivado = '{}' WHERE idtarea = '{}'".format(valor, ide)
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception:
+            print("No se pudo archivar la tarea")
+            raise
+
+    def all_task_arch (self):
+        query ='SELECT * FROM tareas WHERE archivado = 1 ORDER BY prioridad_idprioridad, fecha_fin;'
+
+        self.cursor.execute(query)
+        tareas=self.cursor.fetchall()
+        tareas=list(tareas)
+        return tareas
+    
     def delete_tarea(self, ide):
         query = "DELETE FROM tareas WHERE idtarea = '{}'".format(ide)
         try:
@@ -91,7 +109,7 @@ class Database():
         except Exception:
             print("No se pudo eliminar la tarea")
             raise
-    
+
     def close(self):
         self.connection.close()
 
